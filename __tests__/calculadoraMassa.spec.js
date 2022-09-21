9b9f86001ba358dd8cad34ea17ddbb48d24ec365
Rafael Correia <78096498+rafafcorreia@users.noreply.github.com>
2022-09-21 20:22:06 -0300

    Add files via upload

---
+++ __tests__/calculadoraMassa.spec.js
@@ -0,0 +1,43 @@
+const calculadora = require('../src/calculadora');
+
+//Importa a massa
+const massa = require('../data/massa')
+
+//Soma
+test.each(massa.array.map(elemento => [
+    elemento.a, 
+    elemento.b, 
+    elemento.esperadoSoma]))
+    ('somar %f + %f com massa', (a, b, esperado) => {
+
+    expect(calculadora.somarDoisNumeros(a, b)).toBe(esperado);
+});
+
+//Subtração
+test.each(massa.array.map(elemento => [
+    elemento.a, 
+    elemento.b, 
+    elemento.esperadoSubtracao]))
+    ('subtrair %f + %f com massa', (a, b, esperado) => {
+
+    expect(calculadora.subtrairDoisNumeros(a, b)).toBe(esperado);
+});
+
+//Subtração
+test.each(massa.array.map(elemento => [
+    elemento.a, 
+    elemento.b, 
+    elemento.esperadoMultiplicacao]))
+    ('somar %f + %f com massa', (a, b, esperado) => {
+
+    expect(calculadora.multiplicarDoisNumeros(a, b)).toBe(esperado);
+});
+
+//Divisão
+test.each(massa.array.map(elemento => [
+    elemento.a,
+    elemento.b,
+    elemento.esperadoDivisao]))
+    ('dividir %f / %f com massa', (a, b, esperado) => {
+    expect(calculadora.dividirDoisNumeros(a, b)).toBe(esperado);
+});
