d441880c6231125be47ba4ae373e658582ec1638
Rafael Correia <78096498+rafafcorreia@users.noreply.github.com>
2022-09-21 20:22:40 -0300

    Add files via upload

---
+++ massa.js
@@ -0,0 +1,34 @@
+module.exports.array = [
+  {
+    "a": 1,
+    "b": 2,
+    "esperadoSoma": 3,
+    "esperadoSubtracao": -1,
+    "esperadoMultiplicacao": 2,
+    "esperadoDivisao": 0.5
+  },
+  {
+    "a": 3,
+    "b": 7,
+    "esperadoSoma": 10,
+    "esperadoSubtracao": -4,
+    "esperadoMultiplicacao": 21,
+    "esperadoDivisao": 0.42857142857142855
+  },
+  {
+    "a": 5,
+    "b": 8,
+    "esperadoSoma": 13,
+    "esperadoSubtracao": -3,
+    "esperadoMultiplicacao": 40,
+    "esperadoDivisao": 0.625
+  },
+  {
+    "a": 80,
+    "b": 20,
+    "esperadoSoma": 100,
+    "esperadoSubtracao": 60,
+    "esperadoMultiplicacao": 1600,
+    "esperadoDivisao": 4
+  }
+]
